e8981393edb272a3dba88ee940d242fc6be34219
Sreejagoud <102413780+Sreejagoud@users.noreply.github.com>
2022-04-29 20:01:05 +0530

    Add files via upload

---
+++ preparesdsStatementJDBCProject/src/com/std/DbConnect.java
@@ -0,0 +1,28 @@
+package com.std;
+
+
+		import java.sql.Connection;
+		import java.sql.DriverManager;
+
+		public class DbConnect {
+		static String driver="com.mysql.cj.jdbc.Driver";
+		static String url="jdbc:mysql://localhost:3306/mydb";
+		static String un = "root";
+		static String pass= "root";
+		static Connection conn=null;
+		public static Connection getConnection() {
+			
+			try {
+				Class.forName(driver);
+				conn=DriverManager.getConnection(url,un,pass);
+				if(conn==null) {
+					System.out.println("Connection object is null");
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			return conn;
+			
+		}	
+		}
+	
