7019a4e98c08a2b4b22db3e40bf41a4a3289f637
Sreejagoud <102413780+Sreejagoud@users.noreply.github.com>
2022-05-08 10:01:06 +0530

    Add files via upload

---
+++ UserWebLogin/src/main/java/com/sree/UserLogin.java
@@ -0,0 +1,45 @@
+package com.sree;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserLogin
+ */
+@WebServlet("/UserLogin")
+public class UserLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out=response.getWriter();
+		String un=request.getParameter("uname");
+		String up=request.getParameter("upass");
+	   if(un.equalsIgnoreCase("sreeja") && up.equals("sreeja123")) 
+	   {
+		   out.println("user is VALID");
+	   }
+	   else
+	   {
+		   out.println("user is INVALID");
+	   }
+	}
+}
